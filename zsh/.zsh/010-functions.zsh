cd46f0ff98e98938f3ff5377b35c713537467540
David Bagonyi <david.bagonyi@gmail.com>
2015-06-19 13:00:38 +0100

    Add mkcd function

--- zsh/.zsh/010-functions.zsh
+++ zsh/.zsh/010-functions.zsh
@@ -51,3 +51,15 @@ function ftag {
   tag=$(echo "$tags" | fzf-tmux -d 15 +m) &&
   git checkout $(echo "$tag")
 }
+
+# http://unix.stackexchange.com/questions/9123/is-there-a-one-liner-that-allows-me-to-create-a-directory-and-move-into-it-at-th
+mkcd () {
+  case "$1" in
+    */..|*/../) cd -- "$1";; # that doesn't make any sense unless the directory already exists
+    /*/../*) (cd "${1%/../*}/.." && mkdir -p "./${1##*/../}") && cd -- "$1";;
+    /*) mkdir -p "$1" && cd "$1";;
+    */../*) (cd "./${1%/../*}/.." && mkdir -p "./${1##*/../}") && cd "./$1";;
+    ../*) (cd .. && mkdir -p "${1#.}") && cd "$1";;
+    *) mkdir -p "./$1" && cd "./$1";;
+  esac
+}

62ed0f65f5f913b8c86fbfc7d38a097822c6b0e0
David Bagonyi <david.bagonyi@gmail.com>
2015-08-22 21:07:48 +0100

    Add macports and pip zsh plugins

--- zsh/.zsh/000-antigen.zsh
+++ zsh/.zsh/000-antigen.zsh
@@ -6,6 +6,8 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle docker
 antigen bundle boot2docker
 antigen bundle httpie
+antigen bundle macports
+antigen bundle pip
 
 antigen theme ys
 

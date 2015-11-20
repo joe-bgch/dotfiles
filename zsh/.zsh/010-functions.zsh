23d06e0acda81f5addc3f29658772476f2274113
David Bagonyi <david.bagonyi@gmail.com>
2015-11-20 15:27:44 +0000

    Move tmpdir to functions.zsh

--- zsh/.zsh/010-functions.zsh
+++ zsh/.zsh/010-functions.zsh
@@ -171,3 +171,23 @@ fix-ssh() {
     fi
   done
 }
+
+# https://github.com/mafintosh/tmpdir-tab
+function tmpdir {
+    [[ "$TMPDIR_TAB" == "" ]] && export TMPDIR_TAB="$(mkdir -p /tmp/tmpdir && mktemp -d /tmp/tmpdir/XXXXXX)"
+
+    if [ -t 1 ]; then
+        cd "$TMPDIR_TAB"
+    else
+        echo "$TMPDIR_TAB"
+    fi
+}
+
+function _tmpdir_on_exit {
+    if [ "$TMPDIR_TAB" != "" ] && [ -d "$TMPDIR_TAB" ]; then
+        rm -rf "$TMPDIR_TAB"
+        rmdir /tmp/tmpdir &>/dev/null || true
+    fi
+}
+
+trap _tmpdir_on_exit EXIT

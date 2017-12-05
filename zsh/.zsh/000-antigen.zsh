504c35e65466af6cc17acb50921383d7f29efdc2
David Bagonyi <david.bagonyi@gmail.com>
2017-12-05 17:25:26 +0000

    Use less antigen plugins

--- zsh/.zsh/000-antigen.zsh
+++ zsh/.zsh/000-antigen.zsh
@@ -3,15 +3,6 @@ source ~/.zsh/antigen/antigen.zsh
 antigen use oh-my-zsh
 
 antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle docker
-antigen bundle boot2docker
-antigen bundle httpie
-antigen bundle macports
-antigen bundle pip
-antigen bundle dirhistory
-antigen bundle micha/resty
-antigen bundle jamesob/desk shell_plugins/zsh
-antigen bundle Tarrasch/zsh-autoenv
 
 antigen theme ys
 

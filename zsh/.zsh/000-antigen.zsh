956d56a225d4095f8dd24422492cc4686c207155
David Bagonyi <david.bagonyi@gmail.com>
2018-02-21 12:23:52 +0000

    Source antigen from brew installation

--- zsh/.zsh/000-antigen.zsh
+++ zsh/.zsh/000-antigen.zsh
@@ -1,9 +1,13 @@
-source ~/.zsh/antigen/antigen.zsh
+if [ -f /usr/local/share/antigen/antigen.zsh ]; then
+    source /usr/local/share/antigen/antigen.zsh
 
-antigen use oh-my-zsh
+    antigen use oh-my-zsh
 
-antigen bundle zsh-users/zsh-syntax-highlighting
+    antigen bundle zsh-users/zsh-syntax-highlighting
 
-antigen theme ys
+    antigen theme ys
 
-antigen apply
+    antigen apply
+else
+    echo "antigen does not seem to be installed. Run 'brew install --HEAD antigen'"
+fi

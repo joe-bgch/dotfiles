c646d73ea5b813643d7b908f8bb900287918cb7b
David Bagonyi <david.bagonyi@gmail.com>
2018-09-19 16:41:30 +0100

    Add more zplug plugins

--- zsh/.zsh/000-zplug.zsh
+++ zsh/.zsh/000-zplug.zsh
@@ -2,10 +2,25 @@ export ZPLUG_HOME=/usr/local/opt/zplug
 source ${ZPLUG_HOME}/init.zsh
 
 if [ -f ${ZPLUG_HOME}/init.zsh ]; then
-    zplug "zsh-users/zsh-syntax-highlighting"
+    zplug "zsh-users/zsh-syntax-highlighting", from:github
+    zplug "zsh-users/zsh-completions", from:github
+    zplug "zsh-users/zsh-history-substring-search", from:github
+
     zplug "themes/ys", from:oh-my-zsh
 
+    if ! zplug check --verbose; then
+        printf "Install new zplug plugins? [y/N]: "
+        if read -q; then
+            echo; zplug install
+        fi
+    fi
+
     zplug load
+
+    if zplug check zsh-users/zsh-history-substring-search; then
+        bindkey '^[[A' history-substring-search-up
+        bindkey '^[[B' history-substring-search-down
+    fi
 else
     echo "zplug does not seem to be installed. Run 'brew install zplug'"
 fi

edc6eba4cce9e72415adce839f62313b5a401254
David Bagonyi <david.bagonyi@gmail.com>
2018-09-19 16:41:07 +0100

    Make zsh history work again

---
+++ zsh/.zsh/030-history.zsh
@@ -0,0 +1,4 @@
+HISTSIZE=99999          # How many lines of history to keep in memory
+HISTFILE=~/.zsh_history # Where to save history to disk
+SAVEHIST=99999          # Number of history entries to save to disk
+setopt appendhistory    # Append history to the history file (no overwriting)
